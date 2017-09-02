3bfe832cac6f77dd8538e0eaf58dd9203d691c99
HeerKirov <HeerKirov@outlook.com>
2017-09-02 18:08:10 +0800

    为提交做了一个错误处理。

--- Client/sdustoj_client/rest_api/submissions.py
+++ Client/sdustoj_client/rest_api/submissions.py
@@ -1,6 +1,7 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from config import CLIENT_SETTINGS
+from json.decoder import JSONDecodeError
 
 
 url = CLIENT_SETTINGS['root_url'].rstrip('/') + '/' + CLIENT_SETTINGS['submission_url'].lstrip('/')
@@ -11,11 +12,12 @@ auth = HTTPBasicAuth(username, password)
 
 def request_submit(submission_json):
     request_url = '%s/' % (url.rstrip('/'),)
-    r = requests.post(url, submission_json, auth=auth)
-    if r.status_code == 201:
-        return r.json()
-    else:
-        return None
+    r = requests.post(url, json=submission_json, auth=auth)
+    try:
+        result = r.status_code == 201, r.json()
+    except JSONDecodeError:
+        result = False, {'error': 'Internal Server Error.'}
+    return result
 
 
 def submit(submission_json):
@@ -25,11 +27,12 @@ def submit(submission_json):
     :return: 
     """
     chance_left = 3
-    result = None
-    while result is None and chance_left > 0:
+    result = False
+    json = None
+    while result is False and chance_left > 0:
         print('submitting submission, tried %s ......' % (3 - chance_left,))
-        result = request_submit(submission_json)
+        result, json = request_submit(submission_json)
         chance_left -= 1
-    if result is None:
+    if result is False:
         print('submitting submission failed')
-    return result
+    return result, json
